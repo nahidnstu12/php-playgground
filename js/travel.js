455c6a84c959338221febf96ae3724a72c46f094
nahidnstu12 <nahid.dev19@gmail.com>
2022-05-30 18:03:54 +0600

    travel page

---
+++ js/travel.js
@@ -0,0 +1,22 @@
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 100) {
+    nav.classList.add('bg-dark', 'shadow');
+  } else {
+    nav.classList.remove('bg-dark', 'shadow');
+  }
+});
+
+/*==================== SWIPER DISCOVER ====================*/
+let swiper = new Swiper(".discover-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0,
+    },
+})
